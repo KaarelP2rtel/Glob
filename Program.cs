829677d05d627fde41d71acac5eaa12949a5c895
Kaarel Pärtel <kaarelp2rtel@gmail.com>
2018-03-29 11:02:47 +0300

    Enabled database seeding. Reworked main page design.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Glob.Data;
+using Glob.Models;
+using Helpers;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Glob
@@ -14,7 +19,27 @@ namespace Glob
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    AppDataSeeder.InitializeAppDatabase(context);
+                    AppDataSeeder.InitializeIdentity(userManager, roleManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error ocurred while seeding initial application data!");
+                }
+            }
+            host.Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
